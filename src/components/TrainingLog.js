cecd844e20e73ec62d298673c5a1b06679c5732a
Andy Vo <andy.v.vo2004@gmail.com>
2024-04-10 22:41:00 -0400

    fix training log functionality

--- src/components/TrainingLog.js
+++ src/components/TrainingLog.js
@@ -143,6 +143,7 @@ const TrainingLogForm = ({ open, handleClose, editingLogId }) => {
         method: "GET"
       })
       let temp = await res.json(); //map to an html element and put into drop down
+      console.log(temp);
       const options = temp.filter((animal) => animal.ownerId === userId)
         .map ((animal) => (
         <option key={animal._id} value={animal._id}>{animal.name} - {animal.breed}</option>
